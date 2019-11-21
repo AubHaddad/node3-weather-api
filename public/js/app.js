9f10d29062f807e50d387e14f1de47f1a6e3dbd4
Ayoub HADDAD <aub.haddad@gmail.com>
2019-11-21 11:41:58 +0100

    delete public folder

--- public/js/app.js
+++
@@ -1,1 +0,0 @@
-console.log('Client Side java script file');
--- src/app.js
+++ src/app.js
@@ -30,7 +30,6 @@ app.get('/weather', (req, res) => {
     })
 
 })
-
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 })
